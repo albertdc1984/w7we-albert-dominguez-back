534fff4c238f52d838782d7d87cc7a7ac968f1ca
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-26 16:02:48 +0100

    Created user model

---
+++ src/database/models/User.js
@@ -0,0 +1,31 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  friends: {
+    type: [Schema.Types.ObjectId],
+    required: false,
+  },
+  enemies: {
+    type: [Schema.Types.ObjectId],
+    required: false,
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+module.exports = User;
