95e38aa2fb65db8f62fbe6dda1543d6b414ddbbf
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-26 14:41:12 +0100

    Moved databaseConnect function from index to database folder and imported back to index

--- src/database/index.js
+++ src/database/index.js
@@ -1,1 +1,17 @@
-const serverUp = (app, port) => {};
+const mongoose = require("mongoose");
+const debug = require("debug")("social:database:");
+const chalk = require("chalk");
+
+const databaseConnect = (mongoConnection) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(mongoConnection, (error) => {
+      if (error) {
+        reject(new Error(`Couldn't connect to the database: ${error.message}`));
+        return;
+      }
+      debug(chalk.redBright("Connected to Database"));
+      resolve();
+    });
+  });
+
+module.exports = databaseConnect;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 require("dotenv").config();
+const chalk = require("chalk");
 const express = require("express");
-const { default: mongoose } = require("mongoose");
 const debug = require("debug")("social:");
+const cors = require("cors");
+const databaseConnect = require("./database");
 
 const serverUp = require("./server");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 4000;
 const mongoConnection = process.env.MONGO_STRING;
 
-const databaseConnect = () =>
-  new Promise((resolve, reject) => {
-    mongoose.connect(mongoConnection, (error) => {
-      if (error) {
-        reject(new Error(`Couldn't connect to the database: ${error.message}`));
-        return;
-      }
-      debug("Connected to DB");
-      resolve();
-    });
-  });
-
-serverUp(app, port);
-databaseConnect();
+(async () => {
+  try {
+    await databaseConnect(mongoConnection);
+    await serverUp(app, port);
+  } catch (error) {
+    debug(chalk.bgRed.white(error.message));
+  }
+})();
